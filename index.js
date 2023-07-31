3bb4e459b05795d311ee6c6ebd631f4dad3cdfe3
kmc369 <keishacoleman710@gmail.com>
2023-07-31 18:32:46 -0400

    SequelizeandPostGRE Not Complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app= express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
-app.listen(5000, ()=>{
-    console.log('server running')
+app.listen(port, ()=>{
+    console.log(`server running ${port}`)
 })
