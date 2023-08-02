86c785c266bb4a239a18cbc98d45ebbe90c46a89
kmc369 <keishacoleman710@gmail.com>
2023-08-02 16:59:30 -0400

    Error handling

--- backend/app.js
+++ backend/app.js
@@ -56,4 +56,32 @@ app.use((_req, _res, next) => {
   err.status = 404;
   next(err);
 });
+
+const { ValidationError } = require('sequelize');
+
+// ...
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 module.exports = app;
