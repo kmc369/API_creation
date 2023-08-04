a5f0ba50bafce9faad13a4f0a89c388cfde054a8
kmc369 <keishacoleman710@gmail.com>
2023-08-04 08:33:12 -0400

    Seeding Error

--- backend/db/migrations/20230802233439-create-user.js
+++ backend/db/migrations/20230802233439-create-user.js
@@ -1,5 +1,10 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
@@ -19,16 +24,16 @@ module.exports = {
      
       },
       username: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(30),
         allowNull:false,
         unique:true
       },
       hashedPassword: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING.BINARY,
         allowNull:false
       },
       email: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(256),
         allowNull:false,
         unique:true,
       
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -47,23 +47,38 @@ module.exports = (sequelize, DataTypes) => {
       allowNull:false,
       unique:true,
       validate:{
-        len:[3,30]
+        len:[4,30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error("Cannot be an email.");
+          }
+        }
       }
     },
     hashedPassword: {
-      type:DataTypes.STRING,
-      allowNull:false
+      type:DataTypes.STRING.BINARY,
+      allowNull:false,
+      validate: {
+        len: [60, 60]
+      }
     },
     email: {
       type:DataTypes.STRING,
+      allowNull:false,
       unique:true,
       validate:{
-        isEmail:true
+        isEmail:true,
+        len:[3,256]
       }
     }
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+      }
+    }
   });
   return User;
 };
