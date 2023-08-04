344dfb8fba75c6722be08266a3d9b4e3ec3cce8b
kmc369 <keishacoleman710@gmail.com>
2023-08-04 09:57:18 -0400

    validateSignup

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -75,10 +75,7 @@ router.post('/',async (req, res, next) => {
     handleValidationErrors
   ];
 
-  router.post(
-    '/',
-    validateLogin,
-    async (req, res, next) => {
+  router.post('/', validateLogin,async (req, res, next) => {
       const { credential, password } = req.body;
   
       const user = await User.unscoped().findOne({
