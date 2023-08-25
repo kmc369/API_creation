ee9ea486e511161333a1a8e1a257e877a8756fc2
kmc369 <keishacoleman710@gmail.com>
2023-08-25 10:44:10 -0400

    functional

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
-import Navigation from "./components/Navigation";
-
+// import Navigation from "./components/Navigation";
+import Navigation from '../src/components/Navigation'
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
