52c14b16f9ca15ffac19e7691ba0f194aee958f9
kmc369 <keishacoleman710@gmail.com>
2023-08-28 21:15:02 -0400

    Details almost done

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -2,20 +2,47 @@ import React, { useEffect} from 'react'
 import { useDispatch ,useSelector} from 'react-redux'
 import * as DetailActions from '../../store/details'
 import { useParams } from 'react-router-dom'; 
+import './SpotDetails.css';
 
 export default function SpotDetails({match}) {
   const spotDetail = useSelector(state=>state.details.spot)
   const dispatch = useDispatch()
 
   const { spotId } = useParams() // Extract spotId from URL params
-  console.log(spotId)
+ 
   useEffect(() => {
+ 
     dispatch(DetailActions.getSpotDetailsThunk(spotId)); // Pass spotId to action creator
   }, [dispatch, spotId]);
 
+  if(Object.values(spotDetail).length===0 ){
+  
+    return null
+  }
+
+ 
+
 
   return (
-    <div>SpotDetails Hello</div>
+    <>
+    <div className='spotDetails'>
+      <h1>{ spotDetail.name}</h1>
+      <p>{spotDetail.city}, {spotDetail.state}, {spotDetail.country}</p>
+    </div>
+
+
+    <div className='detailImages'>
+      {spotDetail.SpotImages.map((element,index)=>(
+        <img id={`s${index}`} src={element.url} alt="img"></img>
+       
+     
+
+      ))} 
+       </div>
+
+    <p>Hosted By: {spotDetail.Owner.firstName},{spotDetail.Owner.lastName}</p>
+    <p>description {spotDetail.description}</p>
+    </>
   )
 }
 
