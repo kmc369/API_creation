1f8a6537c864cfe365af6c6aaf2bce645cb7dfaf
kmc369 <keishacoleman710@gmail.com>
2023-08-30 09:46:13 -0400

    created a spot

--- frontend/src/store/createspot.js
+++ frontend/src/store/createspot.js
@@ -2,46 +2,64 @@ import { csrfFetch } from "./csrf"
 const CREATE_SPOT = 'create/spot'
 
 export const actionCreateSpot = (spot) => {
-    return { type: CREATE_SPOT, 
+    return { 
+            type: CREATE_SPOT, 
                 payload: spot 
             };
   };
 
 
-  export const getAllSpotsThunk = (spot)=> async (dispatch) =>{
-      const   {country,address,city,state,lat,long,text,title,price,livephoto1,livephoto2,livephoto3,livephoto4,livephoto5} = spot
-    try{
-    const response = await csrfFetch('api/spots',{
-        method:'POST',
-        body:{
-            country,
-            address,
-            city,
-            state,
-            lat,
-            long,
-            text,
-            title,
-            price,
-            livephoto1,
-            livephoto2,
-            livephoto3,
-            livephoto4,
-            livephoto5  
-        }
-    })
+  export const createSpotThunk = (spot) => async (dispatch) => {
+    const {
+      address,
+      city,
+      country,
+      description,
+      latitude,
+      longitude,
+      price,
+      spotTitle,
+      state,
+     
+     
+    } = spot;
 
-    if(response.ok){
-        const data = await response.json()
-        dispatch(actionCreateSpot(data))
-        // console.log(data)
-        return data
+    console.log(spot)
+    try {
+      
+      const response = await csrfFetch('/api/spots', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({
+            address:address,
+            city:city,
+            state:state,
+            country:country,
+            lat:latitude,
+            lng:longitude,
+            name:spotTitle,
+            description:description,
+            price:price,
+          // livephoto1,
+          // livephoto2,
+          // livephoto3,
+          // livephoto4,
+          // livephoto5
+        }),
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log("reponse from the thunk", data)
+        dispatch(actionCreateSpot(data));
+        return data;
+      } 
+    } catch (error) {
+      console.error('An error occurred:', error.message);
     }
-    }catch(error){
-        console.error("An error occurred:", error);
-    }
-}
-
+  };
 const initialState ={}
 export const createSpotReducer = (state=initialState,action)=>{
 
