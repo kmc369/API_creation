901872206bf19bc121f04a3bae8e95edf1dc3cd2
kmc369 <keishacoleman710@gmail.com>
2023-08-31 15:14:40 -0400

    spot details back

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -13,10 +13,11 @@ import OpenModalButton from '../OpenModalButton'
 export default function SpotDetails() {
   const spotDetail = useSelector(state=>state.spots.spotDetails)
 
-  console.log("spot details are here baby", spotDetail)
 
   
   const reviewDetails = useSelector((state) => state.reviews.spot);
+  console.log("revieww details are here baby", reviewDetails)
+
   const currentUser = useSelector(state => state.session.user)
   
   
@@ -77,12 +78,15 @@ const monthYearFormat = `${months[date.getUTCMonth()]} ${date.getUTCFullYear()}`
 return monthYearFormat;
 }
 
+const value = Object.values(reviewDetails )
 
-// const hasPostedReview = reviewDetails.Reviews.some(
-//   review => review.User.id === currentUser.id && review.spotId === spotId
-// );
+const hasPostedReview = value.some(
+  review => review.User.id === currentUser.id && review.spotId === spotId
+);
 
 const isSpotOwner = spotDetail.Owner.id === currentUser.id;
+
+
 
   return (
     <>
@@ -116,7 +120,7 @@ const isSpotOwner = spotDetail.Owner.id === currentUser.id;
    <div className='reviewsContainer'>
     
   <h1><i className="fa-solid fa-star"></i>{spotDetail.avgStarRating} {formatReviewCount(spotDetail.numReviews)}</h1>
-  {/* <div>
+  <div>
           {currentUser && !hasPostedReview && !isSpotOwner && (
             // Use OpenModalButton to open the modal with ReviewForm content
             <OpenModalButton
@@ -126,9 +130,9 @@ const isSpotOwner = spotDetail.Owner.id === currentUser.id;
               
             />
           )}
-        </div>  */}
+        </div> 
        
-   {/* {reviewDetails.Reviews
+    {value
     .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)) 
     .map((element, index) => (
       
@@ -137,7 +141,7 @@ const isSpotOwner = spotDetail.Owner.id === currentUser.id;
         <p>{dateFormat(element.createdAt)}</p>
         <p>{element.review}</p>
       </div>
-    ))}      */}
+    ))}      
  </div>
 
 
