68d1e5feb179790acfc936b06786b787d9252ead
kmc369 <keishacoleman710@gmail.com>
2023-08-31 18:25:18 -0400

    deleting spot

--- frontend/src/components/DeleteSpot/index.js
+++ frontend/src/components/DeleteSpot/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch ,useSelector} from 'react-redux'
 import * as SpotActions from '../../store/spot'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
@@ -9,22 +9,23 @@ export const DeleteSpot = ({spotId,onCloseModal}) => {
     // console.log(spotId)
     const dispatch = useDispatch()
     const history= useHistory()
-  
+    const userSpots = useSelector(state =>state.spots.allSpots)
+
 
     const {closeModal} = useModal()
-    const handleDelete = (e) => {
+    const handleDelete = async (e) => {
   
-        dispatch(SpotActions.deleteSpotThunk(spotId));
-        dispatch(SpotActions.getSpotByUserIdThunk())
+       await dispatch(SpotActions.deleteSpotThunk(spotId));
         closeModal()
+       await  dispatch(SpotActions.getSpotByUserIdThunk())
       };
-
+    
   return (
     <div>DeleteSpot
        <div className="delete-spot">
       <h2>Confirm Delete</h2>
       <p>Are you sure you want to remove this spot from the listing?</p>
-      <button type='submit' onClick={handleDelete()} >Yes, Delete Spot</button>
+      <button type='submit' onClick={handleDelete} >Yes, Delete Spot</button>
       <button onClick={closeModal}>No, Keep Spot</button>
     </div>
     </div>
