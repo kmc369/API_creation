956d9095e441688ba6125442c19ffe2a9569ea65
kmc369 <keishacoleman710@gmail.com>
2023-09-04 11:39:40 -0400

    Review Longer

--- backend/db/migrations/20230803002351-create-review.js
+++ backend/db/migrations/20230803002351-create-review.js
@@ -33,7 +33,7 @@ module.exports = {
       
       },
       review: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull:false
       },
       stars: {
--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -32,7 +32,7 @@ module.exports = (sequelize, DataTypes) => {
       type:DataTypes.INTEGER,
     },
     review: {
-      type:DataTypes.STRING,
+      type:DataTypes.TEXT,
     },
     stars: {
       type:DataTypes.INTEGER
